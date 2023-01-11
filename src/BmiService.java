f8cc801c9026fd5130512fe2bb46a1f586a5da55
LuNTIK969 <nasa.969@gmail.com>
2023-01-11 22:04:53 +0300

    code UPD

--- src/BmiService.java
+++ src/BmiService.java
@@ -2,7 +2,6 @@ public class BmiService {
     public double calculate(double height, double weight) {
         double aHeight = height / 100;
         double sqHeight = aHeight * aHeight;
-        double bmi = weight / sqHeight;
-        return bmi;
+        return weight / sqHeight;
     }
 }
